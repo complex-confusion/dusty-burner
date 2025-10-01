23d1d1337283f6779d68910d1abde741908c227a
Complex Confusion <complex@zarathud.org>
2025-10-01 14:36:23 -0400

    style: remove excess spaces

--- schedule-page-example.php
+++ schedule-page-example.php
@@ -8,7 +8,7 @@
 
 <div class="schedule-section">
     <h2>Event Schedule</h2>
-    
+
     <!-- The export button will be automatically added by JavaScript -->
     <?php echo do_shortcode('[dust_schedule layout="list"]'); ?>
 </div>
@@ -21,22 +21,22 @@ jQuery(document).ready(function($) {
     $('#manual-ics-export').on('click', function() {
         var $btn = $(this);
         $btn.prop('disabled', true).text('Generating...');
-        
+
         // Create form and submit to trigger download
         var form = $('<form>', {
             method: 'POST',
             action: dust_events_ajax.ajax_url,
             target: '_blank'
         });
-        
+
         form.append($('<input>', {type: 'hidden', name: 'action', value: 'export_schedule_ics'}));
         form.append($('<input>', {type: 'hidden', name: 'nonce', value: dust_events_ajax.nonce}));
         form.append($('<input>', {type: 'hidden', name: 'event_name', value: ''}));
-        
+
         $('body').append(form);
         form.submit();
         form.remove();
-        
+
         setTimeout(function() {
             $btn.prop('disabled', false).text('📅 Export Schedule to Calendar');
         }, 1000);
